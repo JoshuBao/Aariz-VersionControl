653669b40d62d7549e1dcf9ae9382550610f18b0
Joshua Cheng <joshuacheng888@gmail.com>
2022-09-15 22:52:56 -0700

    Added unit test for tree

--- src/Git/AarizGitTester.java
+++ src/Git/AarizGitTester.java
@@ -12,6 +12,7 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 import org.junit.jupiter.api.AfterAll;
@@ -111,6 +112,30 @@ class AarizGitTester {
 				assertFalse(s.hasNext());//if removed all, would be empty
 				
 	}
+	@Test
+	void treeObjTest() throws IOException {
+		//create arraylist, add elements (blobs and trees)
+		ArrayList<String> list = new ArrayList<String>();
+		list.add("blob : 81e0268c84067377a0a1fdfb5cc996c93f6dcf9f");
+		list.add("blob : 01d82591292494afd1602d175e165f94992f6f5f");
+		list.add("blob : f1d82236ab908c86ed095023b1d2e6ddf78a6d83");
+		list.add("tree : bd1ccec139dead5ee0d8c3a0499b42a7d43ac44b");
+		list.add("tree : e7d79898d3342fd15daf6ec36f4cb095b52fd976");
+		
+		TreeObject theTree = new TreeObject(list);
+		File file = new File("test/objects/dd4840f48a74c1f97437b515101c66834b59b1be");
+		assertTrue(file.exists());
+		
+		//Check if all lines exist and are correct 
+		Scanner scanner = new Scanner(file);
+		assertTrue(scanner.nextLine().equals("blob : 81e0268c84067377a0a1fdfb5cc996c93f6dcf9f"));
+		assertTrue(scanner.nextLine().equals("blob : 01d82591292494afd1602d175e165f94992f6f5f"));
+		assertTrue(scanner.nextLine().equals("blob : f1d82236ab908c86ed095023b1d2e6ddf78a6d83"));
+		assertTrue(scanner.nextLine().equals("tree : bd1ccec139dead5ee0d8c3a0499b42a7d43ac44b"));
+		assertTrue(scanner.nextLine().equals("tree : e7d79898d3342fd15daf6ec36f4cb095b52fd976"));
+		file.delete();
+	}
+
 	
 
 
